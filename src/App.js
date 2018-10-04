fa5b3544454f6aaf94307a9916bb870bce700471
Val <valentin@dufois.fr>
2018-10-03 23:46:27 -0400

    Update to create-react-app v2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,129 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+
+import { withRouter } from 'react-router'
+import { Switch, Route } from 'react-router-dom'
+import api from 'library/api'
+
+import Header from 'scenes/Header'
+import Sidebar from 'scenes/Sidebar'
+import LoginPage from 'scenes/LoginPage'
+import Index from 'scenes/Index'
+import ErrorPopUp from 'scenes/ErrorPopUp'
+
+import { withCookies } from 'react-cookie'
+
+// LOCALIZATIONS
+import { injectIntl } from 'react-intl'
+import Weather from 'scenes/Weather'
+
+require('./style/main.scss')
 
 class App extends Component {
-  render() {
+  constructor (props) {
+    super(props)
+
+    const user = this.props.cookies.get('user') || null
+
+    api.setErrorCallback(this.onNetworkError)
+    api.setUnauthorizedCallback(this.onLogOut)
+
+    this.state = {
+      user: user,
+      error: null,
+      location: window.location,
+      ready: false,
+    }
+  }
+
+  componentDidMount () {
+    this.state.user && this.onLogin(this.state.user)
+
+    this.setState({ ready: true })
+  }
+
+  isUser = () => this.state.user !== null
+
+  onLogin = userInfos => {
+    this.setState({
+      user: {
+        name: userInfos.name,
+        token: userInfos.token,
+      },
+      error: null
+    })
+
+    this.props.cookies.set('user', { name: userInfos.name, token: userInfos.token }, {
+      path: '/',
+      maxAge: 3600 * 4
+    })
+
+    api.setUserToken(userInfos.token)
+  }
+
+  onLogOut = () => {
+    this.props.cookies.remove('user')
+    api.setUserToken('')
+    this.setState({
+      user: null
+    })
+    this.props.history.push('/')
+  }
+
+  onNetworkError = error => {
+    // reset error message if needed, then set the new message
+    this.setState({ error: error })
+  }
+
+  onErrorClose = () => {
+    this.setState({ error: null })
+  }
+
+  dynamics = ['weather']
+
+  dynamicsHomeComponents = {
+    news: null,
+    weather: Weather
+  }
+
+  render () {
+    if (!this.state.ready) return null
+
+    if (!this.isUser()) {
+      if (window.location.pathname !== '/') this.props.history.push('/')
+      return (
+        <main>
+          <Header />
+          <LoginPage
+            key="login-page"
+            onLogin={this.onLogin}/>
+          <ErrorPopUp
+            message={ this.state.error }
+            key="error-popup"
+            onErrorClose={ this.onErrorClose } />
+        </main>
+      )
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <main>
+        <Header />
+        <Switch>
+          <Route exact path="/" render={(props) => <Index {...props} dynamics={this.dynamics} />} />
+          <Route render={props => <Sidebar {...props} dynamics={this.dynamics} />} />
+        </Switch>
+        <Switch>
+          { this.dynamics.map(dynamic => (
+            <Route key={dynamic} path={'/dynamic/' + dynamic} component={this.dynamicsHomeComponents[dynamic]} />
+          )) }
+        </Switch>
+        <ErrorPopUp
+          message={ this.state.error }
+          key="error-popup"
+          onErrorClose={ this.onErrorClose } />
+      </main>
+    )
   }
 }
 
-export default App;
+export default withRouter(withCookies(injectIntl(App)))
--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter } from 'react-router-dom'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//
+// LOCALIZATIONS
+import { IntlProvider, addLocaleData } from 'react-intl'
+import fr from 'react-intl/locale-data/fr'
+import en from 'react-intl/locale-data/en'
+import frenchMessages from './assets/locales/fr_CA'
+import englishMessages from './assets/locales/en_CA'
+
+// ENV
+import { config } from 'dotenv'
+config()
+
+const messages = {
+  'fr-FR': frenchMessages,
+  'en-CA': englishMessages,
+}
+
+addLocaleData([...fr, ...en])
+
+//
+// GO REACT
+
+ReactDOM.render(
+  <IntlProvider
+    locale={ navigator.language }
+    messages={messages[navigator.language]}>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </IntlProvider>, document.getElementById('root'))
+registerServiceWorker()
