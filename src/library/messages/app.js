54eba98c243a791bf260dce751d8983a5a31a1f9
Val <valentin@dufois.fr>
2018-10-16 01:29:59 -0400

    Adding missing messages files

---
+++ src/library/messages/app.js
@@ -0,0 +1,106 @@
+import { defineMessages } from 'react-intl'
+
+export default defineMessages({
+  // Login page
+  loginTitle: {
+    id: 'dynamics.login.title',
+    description: 'Message to greet visitors',
+    defaultMessage: 'Please log in.',
+  },
+  loginEmail: {
+    id: 'dynamics.login.email',
+    description: 'Placeholder for the email input field',
+    defaultMessage: 'e-Mail address',
+  },
+  loginPassword: {
+    id: 'dynamics.login.password',
+    description: 'Placeholder for the password input field',
+    defaultMessage: 'Password',
+  },
+  loginButton: {
+    id: 'dynamics.login.button',
+    description: 'Text displayed on the log-in button',
+    defaultMessage: 'Log in',
+  },
+
+  // Languages
+  english: {
+    id: 'dynamics.common.english',
+    description: 'The English language',
+    defaultMessage: 'English',
+  },
+  french: {
+    id: 'dynamics.common.french',
+    description: 'The French language',
+    defaultMessage: 'French',
+  },
+
+  // Canada & provinces
+  newBrunswick: {
+    id: 'dynamics.provinces.new-brunswick',
+    description: 'Canadian province: New Brunswick',
+    defaultMessage: 'New Brunswick',
+  },
+  labrador: {
+    id: 'dynamics.provinces.labrador',
+    description: 'Canadian province: Newfoundland and Labrador',
+    defaultMessage: 'Newfoundland and Labrador',
+  },
+  princeEdward: {
+    id: 'dynamics.provinces.prince-edward',
+    description: 'Canadian province: Prince Edward Island',
+    defaultMessage: 'Prince Edward Island',
+  },
+  novaScotia: {
+    id: 'dynamics.provinces.novia-scotia',
+    description: 'Canadian province: Nova Scotia',
+    defaultMessage: 'Nova Scotia',
+  },
+  quebec: {
+    id: 'dynamics.provinces.quebec',
+    description: 'Canadian province: Quebec',
+    defaultMessage: 'Quebec',
+  },
+  ontario: {
+    id: 'dynamics.provinces.ontario',
+    description: 'Canadian province: Ontario',
+    defaultMessage: 'Ontario',
+  },
+  manitoba: {
+    id: 'dynamics.provinces.manitoba',
+    description: 'Canadian province: Manitoba',
+    defaultMessage: 'Manitoba',
+  },
+  saskatchewan: {
+    id: 'dynamics.provinces.saskatchewan',
+    description: 'Canadian province: Saskatchewan',
+    defaultMessage: 'Saskatchewan',
+  },
+  alberta: {
+    id: 'dynamics.provinces.alberta',
+    description: 'Canadian province: Alberta',
+    defaultMessage: 'Alberta',
+  },
+  britishColumbia: {
+    id: 'dynamics.provinces.british-columbia',
+    description: 'Canadian province: British-Columbia',
+    defaultMessage: 'Colombie-Britannique',
+  },
+
+  // Misc
+  appSwitcherTitle: {
+    id: 'app-switcher.title',
+    description: 'Title on top of the app switcher, preceding the list of Neo\'s apps',
+    defaultMessage: 'Neo apps'
+  },
+  errorPopupTitle: {
+    id: 'dynamics.error-popup.title',
+    description: 'Error popup title signaling an error',
+    defaultMessage: 'An error as occured',
+  },
+  dynamics: {
+    id: 'dynamics.dynamics.dynamics',
+    description: 'Title of the dynamic section on the home page',
+    defaultMessage: 'Dynamics',
+  },
+})
---
+++ src/library/messages/params.js
@@ -0,0 +1,44 @@
+import { defineMessages } from 'react-intl'
+
+export default defineMessages({
+  params: {
+    id: 'dynamics.params.params',
+    description: 'Title of the parameters section on the home page',
+    defaultMessage: 'Params',
+  },
+  apiKeys: {
+    id: 'dynamics.params.keys',
+    description: 'API keys',
+    defaultMessage: 'Keys',
+  },
+  newAPIKey: {
+    id: 'dynamics.params.keys.new',
+    description: 'New API keys button label',
+    defaultMessage: 'New key',
+  },
+  APIKeyName: {
+    id: 'dynamics.params.keys.name',
+    description: 'API keys name',
+    defaultMessage: 'Key name',
+  },
+  cancel: {
+    id: 'dynamics.params.keys.cancel',
+    description: 'Cancel something',
+    defaultMessage: 'Cancel',
+  },
+  create: {
+    id: 'dynamics.params.create',
+    description: 'Create something',
+    defaultMessage: 'Create',
+  },
+  save: {
+    id: 'dynamics.params.save',
+    description: 'Save somthing',
+    defaultMessage: 'Save',
+  },
+  APIKeyTitleName: {
+    id: 'dynamics.params.keys.titleName',
+    description: 'Name of the key with \'key\' attached',
+    defaultMessage: '{name} API key',
+  },
+})
